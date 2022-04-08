64d42cb2c8de68655497088ff56484db5d6ecda9
Guillermo-maker <gustavoandino2@gmail.com>
2022-04-08 09:04:37 -0300

    Revert "deleting feik commit"
    This reverts commit b87fb1a3d7d7ec84e8a20bb68e90e6e3586f05d5.

--- activs repaso/activr3.py
+++ activs repaso/activr3.py
@@ -1,18 +1,8 @@
-letra = input("Escribe una letra: ")
+letra = input ("Escribe una letra: ")
 largo = int(input("Escriba el largo de la lista: "))
 ancho = int(input("Escriba el ancho de la lista: "))
 
-for i in range(1, ancho + 1):
-    for j in range(1, largo + 1):
-        print(letra, end="")
-    print(" ")
-
-letra = input("Escribe una letra: ")
-ancho = int(input("Escriba el largo del triangulo: "))
-
-for i in range(ancho):
-    for j in range(ancho- i - 1):
-        print(" ", end="")
-    for j in range(i+1):
-        print(letra, end=" ")
-    print( )
+for i in range (1,ancho +1 ):
+    for j in range (1,largo +1 ):
+        print (letra, end = "")
+    print (" ")
--- activs repaso/activr6.py
+++ activs repaso/activr6.py
@@ -1,17 +1,19 @@
-def contavocal(p):
 
+def contavocal(p):
+      
+   
     contador = 0
 
     vocales = set("aeiouAEIOU")
-
+      
     for alphabet in p:
-
+      
         if alphabet in vocales:
             contador = contador + 1
-
+      
     print("numero de voclales :", contador)
-
+      
 
 p = input("ingresar palabra: ")
-
+  
 contavocal(p)
