a0d07b8c9b534d1dd36ac8a7d1858ab3cd558432
Guillermo-maker <gustavoandino2@gmail.com>
2022-04-22 11:57:19 -0300

    Resto de las funciones y la llamada de los alumnos

--- 2_EjerciciosClases/activ_2.py
+++ 2_EjerciciosClases/activ_2.py
@@ -3,3 +3,25 @@ class Alumno:
     def inicializar(self,nombre,nota):
         self.nombre=nombre
         self.nota=nota
+
+    def imprimir(self):
+        print("Nombre:",self.nombre)
+        print("Nota:",self.nota)
+
+    def mostrar_estado(self):
+        if self.nota>=4:
+            print("Regular")
+        else:
+            print("Libre")
+
+
+
+alumno1=Alumno()
+alumno1.inicializar("Guille",7)
+alumno1.imprimir()
+alumno1.mostrar_estado()
+
+alumno2=Alumno()
+alumno2.inicializar("Juan", 2)
+alumno2.imprimir()
+alumno2.mostrar_estado()
