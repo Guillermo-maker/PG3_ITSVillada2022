225196d86f13649fd4e7e4ee92910b3786e3abf0
Guillermo-maker <gustavoandino2@gmail.com>
2022-05-01 12:05:39 -0300

    activ pre-terminada

--- 2_EjerciciosClases/activ_3.py
+++ 2_EjerciciosClases/activ_3.py
@@ -1,2 +1,32 @@
 class Triangulo:
+
+
+    def inicializar(self, ancho, largo, letra ):
+        self.ancho = ancho
+        self.largo = largo
+        self.letra = letra
+
+    def triangulo(self):
+       for self.largo in range(self.ancho):
+            for j in range(self.ancho- self.largo - 1):
+                    print(" ", end="")
+            for j in range(self.largo+1):
+                print(self.letra, end=" ")
+            print( ) 
+        
+
+    def equilatero(self):
+        if self.ancho == self.largo:
+            print("Es un triangulo equilatero")
+        else:
+            print("No es un triangulo equilatero")
+
+            
+
+t1=Triangulo()
+t1.inicializar(ancho = int(input ("escriba el ancho: ")),largo = int(input ("escriba el largo: ")), letra = input("escriba la letra: "))
+t1.equilatero()
+t1.triangulo()
+
+
     
