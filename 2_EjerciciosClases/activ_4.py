237cba975abfacebb743999c16ab4df3cdddb633
Guillermo-maker <gustavoandino2@gmail.com>
2022-05-05 20:57:51 -0300

    Activs

---
+++ 2_EjerciciosClases/activ_4.py
@@ -0,0 +1,18 @@
+class Operacion:
+
+    def __init__(self):
+        self.valor1=int(input("Ingrese primer valor:"))
+        self.valor2=int(input("Ingrese segundo valor:"))
+        self.sumar()
+        self.restar()
+        self.multiplicar()
+        self.dividir()
+
+    def sumar(self):
+        suma=self.valor1+self.valor2
+        print("La suma es",suma)
+
+    def restar(self):
+        resta=self.valor1-self.valor2
+        print("La rersta es",resta)
+
