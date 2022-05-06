ba8b881a333f53b4b2d8157965e7e73e8bdf6af6
Guillermo-maker <gustavoandino2@gmail.com>
2022-05-06 09:02:20 -0300

    activ_5 terminado :)

--- 2_EjerciciosClases/activ_5.py
+++ 2_EjerciciosClases/activ_5.py
@@ -1,11 +1,29 @@
 class Persona:
-    def __init__(self, nombre, edad):
-        self.nombre = nombre
-        self.edad = edad
+    def __init__(self):
+        self.nombre = input("ingrese el nombre de la persona: ")
+        self.edad = int(input ("ingrese la edad de la persona: "))
 
     def presentar(self):
         print("Hola, soy", self.nombre, "y tengo", self.edad, "años")
     
 
-persona1 = Persona(nombre =input ("ingrese el nombre de la persona: "), edad = int (input ("ingrese la edad de la persona: ")));
+class empleado(Persona):
+    def __init__(self):
+        super().__init__()
+        self.sueldo = int(input("Ingrese el sueldo del empleado: "))
+    
+    def mostrar_sueldo(self):
+        print("El sueldo del empleado es:", self.sueldo)
+
+    def impuestos(self):
+        if self.sueldo >= 3000:
+            print("pagas impuestos:")
+        else:
+            print("no pagas impuestos")
+    
+
+persona1 = Persona()
 persona1.presentar()
+empleado1 = empleado()
+empleado1.mostrar_sueldo()
+empleado1.impuestos()
