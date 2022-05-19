0bec58ffe86a2d4ecbf1ad4a512d04e1d87eb32c
Guillermo-maker <gustavoandino2@gmail.com>
2022-05-19 17:29:11 -0300

    activ_3

---
+++ 3_Ejercicios_Exceptions/activ_3.py
@@ -0,0 +1,18 @@
+while True:
+    try:    
+        meses = ("Enero", "Febrero", "Marzo", "Abril", 'Mayo', 'Junio', 'Julio'
+                ,'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre')
+
+        print (meses)
+
+        pickmes = int (input("elija el mes segun el numero: "))
+
+        print (meses[pickmes - 1])
+        menu = int (input ("para terminar...(0)...para continuar...(1): "))
+        if menu == 0:
+            print ("esto fue el ejercio 2... muchas gracia por su participacion...")
+            break
+
+    except IndexError:
+        print( "ingres el numero del mes correctamente")
+
