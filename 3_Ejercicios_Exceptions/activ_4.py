3de54210147677015fa0980906125fc17d52f820
Guillermo-maker <gustavoandino2@gmail.com>
2022-05-19 17:40:05 -0300

    activ 4

---
+++ 3_Ejercicios_Exceptions/activ_4.py
@@ -0,0 +1,20 @@
+while True:
+
+    try:
+        num1 = int(input ("ingrese el primer numero:"))
+        num2 = int(input ("ingrese el segundo numero:"))
+
+        r = num1 / num2
+
+        print ("el resultado es:", r)
+        menu = int (input ("para terminar...(0)...para continuar...(1): "))
+
+        if menu == 0:
+            print ("esto fue el ejercio 1... muchas gracia por su participacion...")
+            break
+            
+            
+
+    except (ValueError, ZeroDivisionError):
+        print ("ingrese numeros correctamente...no se puede dividir por cero")
+    
