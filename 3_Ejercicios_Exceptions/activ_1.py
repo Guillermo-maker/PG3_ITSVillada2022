103d773daab2206fc3bc9906b8d4b93d3b5dd4f8
Guillermo-maker <gustavoandino2@gmail.com>
2022-05-18 22:08:40 -0300

    Primer Ejercicio

---
+++ 3_Ejercicios_Exceptions/activ_1.py
@@ -0,0 +1,22 @@
+while True:
+
+    try:
+        num1 = int(input ("ingrese el primer numero:"))
+        num2 = int(input ("ingrese el segundo numero:"))
+
+        r = num1 + num2
+
+        print ("el resultado es:", r)
+        menu = int (input ("para terminar...(0)...para continuar...(1): "))
+
+        if menu == 0:
+            print ("esto fue el ejercio 1... muchas gracia por su participacion...")
+            break
+            
+            
+
+    except ValueError:
+        print ("ingrese numeros correctamente...")
+    
+
+
